3d79919b17d3def64242ae126ac385888eb88513
mikce_000 <mikcel@live.com>
2017-08-06 18:31:15 -0400

    Refactored the getAppointments script to get specific information

--- services/getAppointments.php
+++ services/getAppointments.php
@@ -20,11 +20,27 @@ if ($_SESSION["type"] == "DOCTOR" || $_SESSION["type"] == "THERAPIST"){
     $license_no = $_SESSION["username"];
 }
 
+// If need to filter by date
+$start_date = "0000-00-00";
+$end_date = "9999-99-99";
+if (isset($_POST["start_date"])){
+    $start_date = trim($_POST["start_date"]);
+}
+
+if (isset($_POST["end_date"])){
+    $end_date = trim($_POST["end_date"]);
+}
+
 // Query to get all appointments (Add the username depending on the login type)
 $query = "SELECT
+  r.serialno,
   r.phn,
-  CONCAT(p.firstname, ' ', p.lastname) patient,
-  CONCAT(ms.sfirstname, ' ' ,ms.slastname) doctor,
+  p.firstname p_firstname, 
+  p.lastname p_lastname,
+  ms.licenseno,
+  ms.sfirstname staff_firstname,
+  ms.slastname staff_last_name,
+  ms.title staff_title,
   apt.date,
   apt.time
 FROM appointment AS apt
@@ -33,6 +49,10 @@ FROM appointment AS apt
   JOIN patient AS p ON p.phn = r.phn
 WHERE
   ms.licenseno LIKE '$license_no'
+  AND 
+  apt.date >= '$start_date'
+  AND 
+  apt.date <= '$end_date'
 ORDER BY apt.date DESC";
 
 $results = $db_connector->query_assoc($query);
@@ -40,13 +60,12 @@ $results = $db_connector->query_assoc($query);
 // Data array to be returned
 $returned_data = array();
 if ($results){
-    $returned_data["response"] = 1;
+    $returned_data["response"] = true;
     $returned_data["results"] = $results;
 }else{
-    $returned_data["response"] = 0;
+    $returned_data["response"] = false;
     $returned_data["err_message"] = "Unable to fetch data";
 }
 
 // Send data to front-end
 echo json_encode($returned_data);
-
