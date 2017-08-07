97c0e509bed20de2cd48d14b4c644e86448299b3
Erin Benderoff <erinb_17@hotmail.com>
2017-08-07 00:47:55 -0400

    write prescription - added error if appt id not found, added checklist for equipment

--- services/writeprescription.php
+++ services/writeprescription.php
@@ -13,43 +13,67 @@ session_start();
 //connect to db
 $dbc = new DBConnector();
 
-$data = array();
-
 //form data common to both doctor and physio
 $apptid = $_POST['apptid'];
 $diagnosis = $_POST['diagnosis'];
 $notes = $_POST['notes'];
 
-$dbc->query_assoc("INSERT INTO prescription(apptid,diagnosis,notes) VALUES ('$apptid','$diagnosis','$notes')");
+//make sure the appointment id exists
+$result = $dbc->query_assoc("SELECT apptid
+                                    FROM appointment
+                                    WHERE apptid='$apptid'");
+
+//send error msg and exit script if appointment does not exist
+if(!$result) {
+    $data['status'] = 'Error';
+    $data['msg'] = 'Appointment does not exist';
+    echo json_encode($data);
+    exit;
+}
+
+//insert new entry into prescriptions table
+$dbc->query_assoc("INSERT INTO prescription(apptid,diagnosis,notes)
+                    VALUES ('$apptid','$diagnosis','$notes')");
 
 //get the most recently added prescription id
-$result = $dbc->query_assoc("SELECT prescriptionid FROM prescription ORDER BY prescriptionid DESC LIMIT 1");
+$result = $dbc->query_assoc("SELECT prescriptionid
+                              FROM prescription
+                              ORDER BY prescriptionid DESC LIMIT 1");
 $prescriptionid = $result[0]['prescriptionid'];
 
 //doctor prescription
 if($_SESSION['type'] == 'DOCTOR') {
+    //form data only for doctor
     $medication = $_POST['medication'];
 
-    $dbc->query_assoc("INSERT INTO doctorprescription(prescriptionid,medication) VALUES ('$prescriptionid','$medication')");
+    //insert medication into doctor prescription table
+    $dbc->query_assoc("INSERT INTO doctorprescription(prescriptionid,medication)
+                        VALUES ('$prescriptionid','$medication')");
 }
 
-//therapist prescription
+//physiotherapist prescription
 else if($_SESSION['type'] == 'THERAPIST') {
+    //form data only for physiotherapist
     $therapy = $_POST['therapy'];
+
+    //@todo allow for multiple equipments and multiple treatments
     $equipment = $_POST['equipment'];
     $treatment = $_POST['treatment'];
 
-    //@todo find equipment id from equipment name
-    //@todo find treatment id from treatment name
-    $equipmentid = '';
-    $treatmentid = '';
+    //insert therapy, equipment and treatment into physio prescription tables
+    $dbc->query_assoc("INSERT INTO physioprescription(prescriptionid,therapy)
+                        VALUES ('$prescriptionid','$therapy')");
 
-    $dbc->query_assoc("INSERT INTO physioprescription(prescriptionid,therapy) VALUES ('$prescriptionid','$therapy')");
-    $dbc->query_assoc("INSERT INTO phypresequipment(prescriptionid,equipmentid) VALUES ('$prescriptionid','$equipmentid')");
-    $dbc->query_assoc("INSERT INTO physprestreatment(prescriptionid,treatmentid) VALUES ('$prescriptionid','$treatmentid')");
+    //make a loop to insert all equipment/treatment into db
+    $dbc->query_assoc("INSERT INTO phypresequipment(prescriptionid,equipmentid)
+                        VALUES ('$prescriptionid','$equipmentid')");
+    $dbc->query_assoc("INSERT INTO physprestreatment(prescriptionid,treatmentid)
+                        VALUES ('$prescriptionid','$treatmentid')");
 }
 
-$data['msg'] = 'Prescription made successfully!';
+//send a success message to front-end
+$data['status'] = 'Success';
+$data['msg'] = 'Prescription created!';
 echo json_encode($data);
 
 ?>
