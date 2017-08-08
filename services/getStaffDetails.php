be1697a9d659a694ee3d6d565bcd845a237e92a6
mikce_000 <mikcel@live.com>
2017-08-07 23:54:49 -0400

    Added service to get all staff details

---
+++ services/getStaffDetails.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikce_000
+ * Date: 07-Aug-2017
+ * Time: 23:31
+ */
+
+include_once ("../utilities/DBConnector.php");
+
+$returned_data = array("response" => false, "status" => "Error", "msg" => "", "results" => null);
+
+$db_connector = new DBConnector();
+
+$staff_license_no = "%";
+if (array_key_exists("staff_license_no", $_POST) && isset($_POST["staff_license_no"])){
+    $staff_license_no = $_POST["staff_license_no"];
+}
+
+$staff_query = "SELECT 
+    licenseno license_no,
+    sfirstname first_name,
+    slastname last_name
+FROM medicalstaff
+WHERE licenseno LIKE '$staff_license_no';";
+
+$results = $db_connector->query_assoc($staff_query);
+
+if (is_array($results)){
+    if (sizeof($results) > 0){
+        $returned_data["results"] = $results;
+        $returned_data["response"] = true;
+        $returned_data["status"] = "Success";
+    }else{
+        $returned_data["msg"] = "No staff could be found";
+    }
+}else{
+    $returned_data["msg"] = "Unable to fetch staff records";
+}
+
+echo json_encode($returned_data);
